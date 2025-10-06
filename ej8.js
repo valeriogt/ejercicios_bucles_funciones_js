e25bdc5e83b8fe237173a2e2dd7ec54da1006222
valeriogt <vgtvaleriogarcia@gmail.com>
2025-10-06 09:40:24 +0200

    Update ej8.js

---
+++ ej8.js
@@ -0,0 +1,15 @@
+const artists = [
+  { name: 'Kurt Cobain', influences: ['The Beatles', 'Pixies', 'Lead Belly'] },
+  { name: 'David Bowie', influences: ['Little Richard', 'Chuck Berry', 'The Velvet Underground'] },
+  { name: 'Eddie Vedder', influences: ['The Who', 'Neil Young', 'Jim Morrison'] },
+  { name: 'Freddie Mercury', influences: ['Liza Minnelli', 'Jimi Hendrix', 'Aretha Franklin'] },
+  { name: 'John Lennon', influences: ['Elvis Presley', 'Chuck Berry', 'Buddy Holly'] }
+];
+
+for (let i = 0; i < artists.length; i++) {
+  const artist = artists[i].name;
+  const influences = artists[i].influences;
+  for (let j = 0; j < influences.length; j++) {
+    console.log(artist + ' -> ' + influences[j]);
+  }
+}
