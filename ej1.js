0ff4ba64e6364cabadd268f4bbfdc6186da42ca7
valeriogt <vgtvaleriogarcia@gmail.com>
2025-10-06 09:38:51 +0200

    Update ej1.js

---
+++ ej1.js
@@ -0,0 +1,20 @@
+const characters = [
+  { name: 'Luke Skywalker', species: 'Human' },
+  { name: 'Darth Vader', species: 'Human' },
+  { name: 'Chewbacca', species: 'Wookiee' },
+  { name: 'Leia Organa', species: 'Human' },
+  { name: 'R2-D2', species: 'Droid' },
+  { name: 'C-3PO', species: 'Droid' },
+  { name: 'Obi-Wan Kenobi', species: 'Human' },
+  { name: 'Yoda', species: 'Unknown' },
+  { name: 'Han Solo', species: 'Human' }
+];
+const humanCharacters = [];
+
+for (let i = 0; i < characters.length; i++) {
+  if (characters[i].species === 'Human') {
+    humanCharacters.push(characters[i]);
+  }
+}
+
+console.log(humanCharacters);
